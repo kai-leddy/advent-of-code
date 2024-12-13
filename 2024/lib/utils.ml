0a6eeb6b3fc1562b4bb05483d07eda5dbc6db7d5
Kai Leddy <epickaimail@gmail.com>
2024-12-13 15:36:29 +0000

    Add a util for char to/from int

---
+++ 2024/lib/utils.ml
@@ -0,0 +1,29 @@
+let char_to_int (c : char) : int =
+  match c with
+  | '0' -> 0
+  | '1' -> 1
+  | '2' -> 2
+  | '3' -> 3
+  | '4' -> 4
+  | '5' -> 5
+  | '6' -> 6
+  | '7' -> 7
+  | '8' -> 8
+  | '9' -> 9
+  | _ -> raise @@ Invalid_argument "char_to_int: invalid character"
+;;
+
+let int_to_char (i : int) : char =
+  match i with
+  | 0 -> '0'
+  | 1 -> '1'
+  | 2 -> '2'
+  | 3 -> '3'
+  | 4 -> '4'
+  | 5 -> '5'
+  | 6 -> '6'
+  | 7 -> '7'
+  | 8 -> '8'
+  | 9 -> '9'
+  | _ -> raise @@ Invalid_argument "int_to_char: invalid integer"
+;;
