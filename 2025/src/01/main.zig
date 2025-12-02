f90447bc7020df144b45639449bbeac7efb128ba
Kai Leddy <epickaimail@gmail.com>
2025-12-02 23:06:23 +0000

    Setup basic test and embedding input

--- 2025/src/01/main.zig
+++ 2025/src/01/main.zig
@@ -1,14 +1,32 @@
 const std = @import("std");
 
+const input = @embedFile("./input.txt");
+
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    const part1 = getPassword(input);
+
+    std.debug.print("Part 1: {d}\n", .{part1});
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn getPassword(in: []const u8) u8 {
+    return @intCast(in.len);
+}
+
+test "example" {
+    const eg =
+        \\L68
+        \\L30
+        \\R48
+        \\L5
+        \\R60
+        \\L55
+        \\L1
+        \\L99
+        \\R14
+        \\L82
+    ;
+    try std.testing.expectEqual(@as(u8, 3), getPassword(eg));
 }
