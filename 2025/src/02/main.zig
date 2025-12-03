d22058fc3d17594b1f2c822c52d1d2e11f676295
Kai Leddy <epickaimail@gmail.com>
2025-12-03 22:39:07 +0000

    Calculate ranges at comptime without allocator

--- 2025/src/02/main.zig
+++ 2025/src/02/main.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
-fn parseU32(str: []const u8) !u32 {
-    return try std.fmt.parseInt(u32, str, 10);
-}
-
 const input = @embedFile("./input.txt");
 
 pub fn main() !void {
@@ -26,25 +22,24 @@ const Range = struct {
 };
 
 /// Caller owns and must free the returned slice with `allocator.free()`.
-fn splitStringToRanges(alloc: std.mem.Allocator, str: []const u8) ![]Range {
+fn splitStringToRanges(str: []const u8) []Range {
+    const len = std.mem.count(u8, str, &[_]u8{','});
     var segments = std.mem.tokenizeScalar(u8, str, ',');
-    var ranges = try std.ArrayList(Range).initCapacity(alloc, 0);
-    defer ranges.deinit(alloc); // just in case
-    var i: usize = 0;
-    while (segments.next()) |segment| : (i += 1) {
+    var ranges: [len + 1]Range = undefined;
+    for (0..len) |i| {
+        const segment = segments.next().?;
         var iter = std.mem.tokenizeScalar(u8, segment, '-');
-        const first = try parseU32(iter.next().?);
-        const last = try parseU32(iter.next().?);
-        std.debug.print("{d} - {d}\n", .{ first, last });
+        const first = try std.fmt.parseInt(u32, iter.next().?, 10);
+        const last = try std.fmt.parseInt(u32, iter.next().?, 10);
 
-        try ranges.append(alloc, Range{ .first = first, .last = last });
+        ranges[i] = Range{ .first = first, .last = last };
     }
-    return ranges.toOwnedSlice(alloc);
+    return &ranges;
 }
 
-fn invalidIDTotal(alloc: std.mem.Allocator, in: []const u8) !u32 {
-    const ranges = try comptime splitStringToRanges(alloc, in);
-    defer alloc.free(ranges);
+fn invalidIDTotal(comptime in: []const u8) !u32 {
+    @setEvalBranchQuota(10_000);
+    const ranges = comptime splitStringToRanges(in);
 
     // TODO: actually iterate the ranges and find the invalid IDs
 
@@ -52,8 +47,6 @@ fn invalidIDTotal(alloc: std.mem.Allocator, in: []const u8) !u32 {
 }
 
 test "example" {
-    const alloc = std.testing.allocator;
-
     const ex = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-    try std.testing.expectEqual(@as(u32, 1227775554), invalidIDTotal(alloc, ex));
+    try std.testing.expectEqual(@as(u32, 1227775554), invalidIDTotal(ex));
 }
