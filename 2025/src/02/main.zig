4efa946a7aa1d607177cfb56a8a82a1977f05a2c
Kai Leddy <epickaimail@gmail.com>
2025-12-04 22:23:38 +0000

    day 2 part 1

--- 2025/src/02/main.zig
+++ 2025/src/02/main.zig
@@ -3,11 +3,7 @@ const std = @import("std");
 const input = @embedFile("./input.txt");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer gpa.deinit();
-    const alloc = gpa.allocator();
-
-    const part1 = try invalidIDTotal(alloc, input);
+    const part1 = invalidIDTotal(input);
 
     std.debug.print("Part 1: {d}\n", .{part1});
 
@@ -17,36 +13,60 @@ pub fn main() !void {
 }
 
 const Range = struct {
-    first: u32,
-    last: u32,
+    first: u64,
+    last: u64,
 };
 
 /// Caller owns and must free the returned slice with `allocator.free()`.
-fn splitStringToRanges(str: []const u8) []Range {
+fn splitStringToRanges(str: []const u8) []const Range {
     const len = std.mem.count(u8, str, &[_]u8{','});
-    var segments = std.mem.tokenizeScalar(u8, str, ',');
+    @setEvalBranchQuota(len * 1000);
+    var segments = std.mem.tokenizeAny(u8, str, &[_]u8{ ',', '\n' });
     var ranges: [len + 1]Range = undefined;
-    for (0..len) |i| {
+    for (0..(len + 1)) |i| {
         const segment = segments.next().?;
         var iter = std.mem.tokenizeScalar(u8, segment, '-');
-        const first = try std.fmt.parseInt(u32, iter.next().?, 10);
-        const last = try std.fmt.parseInt(u32, iter.next().?, 10);
+        const first = std.fmt.parseInt(u64, iter.next().?, 10) catch unreachable;
+        const last = std.fmt.parseInt(u64, iter.next().?, 10) catch unreachable;
 
         ranges[i] = Range{ .first = first, .last = last };
     }
-    return &ranges;
+    // copy mutable slice to a const to return from comptime
+    const final = ranges;
+    return &final;
 }
 
-fn invalidIDTotal(comptime in: []const u8) !u32 {
-    @setEvalBranchQuota(10_000);
+fn invalidIDTotal(comptime in: []const u8) u64 {
     const ranges = comptime splitStringToRanges(in);
 
-    // TODO: actually iterate the ranges and find the invalid IDs
+    var total: u64 = 0;
 
-    return @intCast(ranges.len);
+    for (ranges) |range| {
+        for ((range.first)..(range.last + 1)) |x| {
+            const id = @as(u64, @intCast(x));
+            if (isInvalidID(id)) {
+                total += id;
+            }
+        }
+    }
+
+    return total;
+}
+
+fn isInvalidID(id: u64) bool {
+    // unlikely that any of the numbers will be more than 64 digits
+    var buf: [64]u8 = undefined;
+    const asStr = std.fmt.bufPrint(&buf, "{}", .{id}) catch {
+        return false;
+    };
+
+    const left = asStr[0..(asStr.len / 2)];
+    const right = asStr[(asStr.len / 2)..asStr.len];
+
+    return std.mem.eql(u8, left, right);
 }
 
 test "example" {
     const ex = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-    try std.testing.expectEqual(@as(u32, 1227775554), invalidIDTotal(ex));
+    try std.testing.expectEqual(@as(u64, 1227775554), invalidIDTotal(ex));
 }
